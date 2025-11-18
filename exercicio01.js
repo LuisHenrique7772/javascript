573b4104da5aeb3a60f2cd3e65495b0c0376f37d
Luis Henrique <47903918842@imaccommand005.local>
2025-11-18 10:58:07 -0300

    exercicio01

---
+++ exercicio01.js
@@ -0,0 +1,12 @@
+let login = prompt("digite o teu usuário")
+
+let senha = prompt("digite a tua senha")
+
+if(login == "admin" && senha == "senha123"){
+
+
+    console.log ("login bem sucedido!")
+}
+else{
+    console.log("voçe não tem permissão de acesso!")
+}
